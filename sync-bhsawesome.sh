f984aed03b8ea47a54fd830f64b2b1501e4ef53a
Peter Seibel <peter@gigamonkeys.com>
2026-04-01 12:50:45 -0700

    Changing dirname of bhsawesome files.

--- sync-bhsawesome.sh
+++ sync-bhsawesome.sh
@@ -2,19 +2,20 @@
 
 set -euo pipefail
 
-BHSAWESOME_ROOT=~/Runestone/books/BHSawesome2
-
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+BHSAWESOME_ROOT=~/Runestone/books/BHSawesome2
+LOCAL_DIR="$SCRIPT_DIR"/bhsawesome
+
+rm -rf "$LOCAL_DIR"
+mkdir -p "$LOCAL_DIR"
 
 cd "$BHSAWESOME_ROOT"
 git pull
 cd pretext
 
-rm -rf "$SCRIPT_DIR"/just-book
-mkdir -p "$SCRIPT_DIR"/just-book
-
 "$SCRIPT_DIR"/list_files.py main.ptx | while read -r f; do
-    cp --parents "$f" "$SCRIPT_DIR"/just-book/;
+    cp --parents "$f" "$LOCAL_DIR";
 done
 
-cp main.ptx "$SCRIPT_DIR"/just-book/
+cp main.ptx "$LOCAL_DIR"
